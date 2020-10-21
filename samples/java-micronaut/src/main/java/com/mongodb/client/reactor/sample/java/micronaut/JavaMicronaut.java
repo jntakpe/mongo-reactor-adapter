008bd3ecceef2599e6ae508c7ff74e6a3838ae51
jntakpe <jntakpe@gmail.com>
2020-10-21 23:48:18 +0200

    refactor: rename java MicronautApplication.java main class to JavaMicronaut.java

--- samples/java-micronaut/src/main/java/com/mongodb/client/reactor/sample/java/micronaut/JavaMicronaut.java
+++ samples/java-micronaut/src/main/java/com/mongodb/client/reactor/sample/java/micronaut/JavaMicronaut.java
@@ -2,9 +2,9 @@ package com.mongodb.client.reactor.sample.java.micronaut;
 
 import io.micronaut.runtime.Micronaut;
 
-public class MicronautApplication {
+public class JavaMicronaut {
 
     public static void main(String[] args) {
-        Micronaut.run(MicronautApplication.class, args).start();
+        Micronaut.run(JavaMicronaut.class, args).start();
     }
 }
