b4b134cfc7493770b9c4909f0b055f23ef837ad3
jntakpe <jntakpe@gmail.com>
2020-11-30 21:57:45 +0100

    feat(#2): init tracing module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,9 @@
 include(":library")
+include(":tracing")
 include("samples:java-springboot")
 include("samples:java-micronaut")
 include("samples:kotlin-micronaut")
 include("samples:kotlin-springboot")
 
 project(":library").name = "mongo-reactor-adapter"
+project(":tracing").name = "tracing-mongo-reactor-adapter"
---
+++ tracing/build.gradle.kts
@@ -0,0 +1,139 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val reactorVersion: String by project
+val mongoDriverVersion: String by project
+val braveVersion: String by project
+val junitVersion: String by project
+val mockkVersion: String by project
+val assertJVersion: String by project
+val testContainersVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    id("org.jetbrains.dokka") version "1.4.10.2"
+    `java-library`
+    `maven-publish`
+    signing
+    jacoco
+}
+
+dependencies {
+    api(project(":mongo-reactor-adapter"))
+    api(platform("io.zipkin.brave:brave-bom:$braveVersion"))
+    api("io.zipkin.brave:brave")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    testImplementation(kotlin("reflect"))
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.assertj:assertj-core:$assertJVersion")
+    testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
+    testImplementation("org.testcontainers:mongodb:$testContainersVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
+}
+
+kotlin {
+    explicitApiWarning()
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+    test {
+        useJUnitPlatform()
+    }
+    dokkaJavadoc.configure {
+        outputDirectory.set(buildDir.resolve("javadoc"))
+    }
+    javadoc {
+        dependsOn(dokkaJavadoc)
+        setDestinationDir(buildDir.resolve("javadoc"))
+    }
+    jacocoTestReport {
+        dependsOn(test)
+        reports {
+            xml.isEnabled = true
+        }
+    }
+    check {
+        dependsOn(jacocoTestReport)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components.findByName("java"))
+            pom {
+                name.set(project.name)
+                description.set("OpenTracing aware MongoDB's Reactor adapter for ReactiveStreams driver")
+                url.set("https://github.com/jntakpe/mongo-reactor-adapter")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jntakpe")
+                        name.set("Jocelyn NTAKPE")
+                    }
+                }
+                issueManagement {
+                    system.set("Github issues")
+                    url.set("https://github.com/jntakpe/mongo-reactor-adapter/issues")
+                }
+                ciManagement {
+                    system.set("Github actions")
+                    url.set("https://github.com/jntakpe/mongo-reactor-adapter/actions")
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:jntakpe/mongo-reactor-adapter.git")
+                    developerConnection.set("scm:git:git@github.com:jntakpe/mongo-reactor-adapter.git")
+                    url.set("https://github.com/jntakpe/mongo-reactor-adapter/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "Maven_central"
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                val sonatypeUsername: String? by project
+                val sonatypePassword: String? by project
+                username = sonatypeUsername
+                password = sonatypePassword
+            }
+        }
+        maven {
+            name = "Github_packages"
+            setUrl("https://maven.pkg.github.com/jntakpe/mongo-reactor-adapter")
+            credentials {
+                val githubActor: String? by project
+                val githubToken: String? by project
+                username = githubActor
+                password = githubToken
+            }
+        }
+    }
+}
+
+signing {
+    if ("CI" in System.getenv()) {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+    sign(publishing.publications["mavenJava"])
+}
